7d8c168b15522a5cd8700a0360b03f173878841e
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2012-12-05 19:41:50 +0100

    Various head start improvements
    * Libraries removed
    * Knockout mapping & validation added
    * Solution rename
    * Nuget auto restore

--- WebApp/App_Start/BundleConfig.cs
+++ WebApp/App_Start/BundleConfig.cs
@@ -8,8 +8,11 @@ namespace OpenCat
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/libraries").Include(
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/knockout-2.2.0.js",
+                        "~/Scripts/knockout.mapping-latest.js",
+                        "~/Scripts/knockout.validation.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
