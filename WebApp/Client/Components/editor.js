f1bf8b6242ee82de92667523a2c7dff70cb58150
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2013-02-02 12:55:17 +0100

    Generic editor component

---
+++ WebApp/Client/Components/editor.js
@@ -0,0 +1,64 @@
+﻿(function (window, $) {
+
+    "use strict";
+
+    var openedEditor = null;
+
+    $('html').on('click.editor', function (event) {
+        if (openedEditor != null) {
+            openedEditor.close();
+        }
+    });
+
+    App.TextEditor = Ember.ContainerView.extend({
+        classNames: ['editor'],
+        childViews: ['viewing'],
+        viewing: Ember.View.create({
+            template: Ember.Handlebars.compile('{{view.parentView.value}}'),
+            click: function (event) {
+                this.get('parentView').startEdit();
+                event.stopPropagation();
+            }
+        }),
+        editing: Ember.View.create({
+            templateName: 'components/editor',
+            click: function (event) {
+                event.stopPropagation();
+            },
+            keyDown: function (event) {
+                if (event.keyCode == 27) {
+                    this.get('parentView').close();
+                } else if (event.keyCode == 13) {
+                    this.get('parentView').save(this.get('value'));
+                }
+            },
+            focus: function () {
+                Ember.run.sync();
+                Ember.run.later(this, function () {
+                    this.$('input').focus();
+                    this.$('input').select();
+                }, 100);
+            }
+        }),
+
+        startEdit: function () {
+            this.removeObject(this.get('viewing'));
+            this.set('editing.value', this.get('value'));
+            this.pushObject(this.get('editing'));
+            this.get('editing').focus();
+            openedEditor = this;
+        },
+        save: function (value) {
+            if (this.get('value') != value) {
+                this.set('value', value);
+            }
+            this.close();
+        },
+        close: function () {
+            this.removeObject(this.get('editing'));
+            this.pushObject(this.get('viewing'));
+            openedEditor = null;
+        }
+    });
+
+})(window, jQuery);
